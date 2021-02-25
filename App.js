571eefa0b8ff73b7ea235d8cf9fce54682a182e3
Your Name <your_email@whatever.com>
2021-02-25 18:52:35 +0300

    two

--- App.js
+++ App.js
@@ -6,16 +6,8 @@ class App extends React.Component
   render()
   {
     return (
-      <View
-        style={{
-          flex:1,
-          justifyContent:"center", alignItems:"center"
-        }}
-      >
-        <Text>Hello World</Text>
-      </View>
+      <View/>
     )
   }
 }
-
 export default App;
